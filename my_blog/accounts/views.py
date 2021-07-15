7d236b5e4fcbe2683ef87baaecb449606f918966
jackie <jackienomsa3@gmail.com>
2021-07-15 19:14:11 +0200

    'templates'

--- my_blog/accounts/views.py
+++ my_blog/accounts/views.py
@@ -1,4 +1,9 @@
+from accounts.models import Login
 from django.shortcuts import render
+from .serializers import LoginSerializer, UserRegSerializer
 
 
 # Create your views here.
+def login(request):
+    form = LoginSerializer()
+    return render(request,'accounts/login.html',{'form':form})
