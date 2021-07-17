c1135086391cc2ab360609f6c480a61b6b4cc8ad
jackie <jackienomsa3@gmail.com>
2021-07-17 18:24:47 +0200

    'forms-working'

--- my_blog/accounts/forms.py
+++ my_blog/accounts/forms.py
@@ -3,13 +3,13 @@ from django import forms
 from rest_framework import serializers
 from .models import Login, User
 
-class LoginForm(forms.Form):
+class LoginForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = Login
         fields = ['username','password']
 
-class UserRegForm(forms.Form):
+class UserRegForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['username','first_name','last_name','email','password1','password2']
+        fields = ['username','first_name','last_name','email','password']
